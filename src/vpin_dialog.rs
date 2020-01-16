1faa15b0f4e913af46aa281388700199139d721d
jlgerber <jlgerber@gmail.com>
2020-01-16 01:38:31 -0800

    interior mut

--- src/vpin_dialog.rs
+++ src/vpin_dialog.rs
@@ -5,13 +5,15 @@ use qt_widgets::{
     cpp_core::{CastInto, MutPtr, Ptr},
     QDialog, QWidget,
 };
+use std::cell::RefCell;
+
 pub struct VpinDialog<'a> {
-    dialog: InnerVpinDialog<'a>,
+    dialog: RefCell<InnerVpinDialog<'a>>,
 }
 
 impl<'a> VpinDialog<'a> {
     pub unsafe fn create(distribution: &str, parent: impl CastInto<MutPtr<QWidget>>) -> VpinDialog {
-        let inner_vpin_dialog = InnerVpinDialog::create(distribution, parent);
+        let inner_vpin_dialog = RefCell::new(InnerVpinDialog::create(distribution, parent));
 
         let dialog = VpinDialog {
             dialog: inner_vpin_dialog,
@@ -22,41 +24,41 @@ impl<'a> VpinDialog<'a> {
     /// Return the accepted signal from the button. This is provided as a convenience
     /// for hooking up a slot from this struct.
     pub unsafe fn accepted(&self) -> Signal<()> {
-        self.dialog.accepted()
+        self.dialog.borrow().accepted()
     }
 
     /// Dismiss the dialog using accept. This is a convenience for consumrs
     /// of this struct, to avoid having to drill down
-    pub unsafe fn accept(&mut self) {
-        self.dialog.accept()
+    pub unsafe fn accept(&self) {
+        self.dialog.borrow_mut().accept()
     }
 
     pub unsafe fn finished(&self) -> Signal<(std::os::raw::c_int,)> {
-        self.dialog.finished()
+        self.dialog.borrow().finished()
     }
     /// Get a ponter to the dialog
     pub fn dialog(&self) -> Ptr<QDialog> {
-        unsafe { self.dialog.dialog() }
+        unsafe { self.dialog.borrow().dialog() }
     }
 
     /// Get a mutable pointer to the dialog
-    pub fn dialog_mut(&mut self) -> MutPtr<QDialog> {
-        unsafe { self.dialog.dialog_mut() }
+    pub fn dialog_mut(&self) -> MutPtr<QDialog> {
+        unsafe { self.dialog.borrow_mut().dialog_mut() }
     }
 
     /// Return the rejected signal
     pub unsafe fn rejected(&self) -> Signal<()> {
-        self.dialog.rejected()
+        self.dialog.borrow().rejected()
     }
 
     /// Return a lsit of selected item names
     pub unsafe fn selected_roles(&self) -> Vec<String> {
-        self.dialog.selected_roles()
+        self.dialog.borrow().selected_roles()
     }
 
     /// Retrieve the current site
     pub unsafe fn selected_site(&self) -> String {
-        self.dialog.selected_site()
+        self.dialog.borrow().selected_site()
     }
 
     /// Return the selected Sequence/shot if applicable
@@ -65,22 +67,22 @@ impl<'a> VpinDialog<'a> {
     }
 
     /// Load the stylesheet
-    pub unsafe fn set_default_stylesheet(&mut self) {
-        self.dialog.set_default_stylesheet();
+    pub unsafe fn set_default_stylesheet(&self) {
+        self.dialog.borrow_mut().set_default_stylesheet();
     }
 
     /// Set the sites
-    pub fn set_sites(&mut self, sites: Vec<&str>) {
-        self.dialog.set_sites(sites);
+    pub fn set_sites(&self, sites: Vec<&str>) {
+        self.dialog.borrow_mut().set_sites(sites);
     }
 
     /// set the list of rols
-    pub fn set_roles(&mut self, roles: Vec<&str>) {
-        self.dialog.set_roles(roles);
+    pub fn set_roles(&self, roles: Vec<&str>) {
+        self.dialog.borrow_mut().set_roles(roles);
     }
 
     /// Given a new LevelMap, repalace the existing one
-    pub fn set_levels(&mut self, levels: LevelMap) {
-        self.dialog.set_levels(levels);
+    pub fn set_levels(&self, levels: LevelMap) {
+        self.dialog.borrow_mut().set_levels(levels);
     }
 }
